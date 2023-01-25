ba6c2d02d1b0f1d85c4437a88a39462969de5dc0
Matheus Cassiano <matheuscassiano6467@gmail.com>
2023-01-25 10:25:26 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model_entities.Department;
+import model_entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
@@ -8,6 +11,9 @@ public class Program {
 		
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		System.out.println(seller);
 		
 		
 		
