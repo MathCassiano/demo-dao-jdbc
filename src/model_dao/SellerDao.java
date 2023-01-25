ee50ede85ea440b7e38a9e90e9816a2ac972ad38
Matheus Cassiano <matheuscassiano6467@gmail.com>
2023-01-25 10:29:41 -0300

    DepartmentDao and SellerDao

---
+++ src/model_dao/SellerDao.java
@@ -0,0 +1,13 @@
+package model_dao;
+
+import model_entities.Seller;
+
+public interface SellerDao {
+	void insert(Seller obj);
+	void update(Seller obj);
+	void deleteById(Integer id);
+	//pega o id e consulta no banco de dados um objeto com esse id, se existir retorna o objeto, se não existir retorna nulo
+	Seller findById(Integer id);
+	List<Seller> findAll();
+
+}
