0848d5ca1e6c0a0e45d584cf600c9300095965b0
Matheus Cassiano <matheuscassiano6467@gmail.com>
2023-01-25 21:25:17 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,18 +9,14 @@ import model_entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
-		
-		
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
-		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-		System.out.println(seller);
-		
-		// meu programa n conhece a implementação, apenas a interface.
+
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
-		
+
+		System.out.println("=== TEST 1 : seller findById ===");
+		Seller seller = sellerDao.findById(3);
+
+		System.out.println(seller);
+
 	}
 
 }
--- src/model_dao/DaoFactory.java
+++ src/model_dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model_dao;
 
+import db.DB;
 import model_dao_impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
 	//ela vai expor um metodo que retorna o tipo da interface, mas internamente vai instanciar uma implementação
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 }
